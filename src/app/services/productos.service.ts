e9f503b7a3443e81c0c66721252e3ff0400d768f
Ruben Marquez <rmarquezg.1978@gmail.com>
2018-03-22 14:17:21 -0700

    Primera versión

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -63,9 +63,9 @@ export class ProductosService {
       this.http.get('https://paginaweb-77735.firebaseio.com/productos_idx.json')
       .subscribe( res => {
 
-        //console.log(res.json());
+        // console.log(res.json());
 
-        //setTimeout( ()=>{
+        // setTimeout( ()=>{
           this.cargando = false;
           this.productos = res.json();
           resolve();
